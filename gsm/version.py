98d3cb77b2e44ffcae67e1f43d7330c13d40d2a7
gabrielcfvg <gabrielcfvg@gmail.com>
2024-02-13 11:42:00 -0300

    fix: project files hash tests

--- gsm/version.py
+++ gsm/version.py
@@ -2,7 +2,7 @@
 import importlib.metadata
 
 # ----------------------------------- local ---------------------------------- #
-from semver import Semver, parse_version
-from log import panic
+from gsm.semver import Semver, parse_version
+from gsm.log import panic
 
 GSM_VERSION: Semver = version if (version := parse_version(importlib.metadata.version('gsm'))) != None else panic("invalid version string")
